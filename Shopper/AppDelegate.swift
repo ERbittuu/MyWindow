6b9f49b6f54943ef4930a15f77dc83085a318914
Utsav Patel <utsav.patel@solutionanalysts.com>
2019-02-16 23:04:16 +0530

    Initial Commit

---
+++ Shopper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Shopper
+//
+//  Created by Utsav Patel on 2/16/19.
+//  Copyright © 2019 erbittuu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Shopper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Shopper
+//
+//  Created by Utsav Patel on 2/16/19.
+//  Copyright © 2019 erbittuu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
